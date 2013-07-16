380db25c46d935fe3b325c8211151bbac54f7a91
eugenyyy <eugenyyy@bigmir.net>
2013-07-16 19:17:28 +0000

    fixed bug with web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send(fs.readFile('index.html');
+  response.send(fs.readFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
